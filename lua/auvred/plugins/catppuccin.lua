b33d1f12d76b4a806daac09a2ad0acc6f4015949
auvred <aauvred@gmail.com>
2024-07-13 16:17:16 +0300

    highlight tweaks

--- lua/auvred/plugins/catppuccin.lua
+++ lua/auvred/plugins/catppuccin.lua
@@ -2,16 +2,10 @@ require("catppuccin").setup({
   flavour = "mocha",
   transparent_background = true,
   show_end_of_buffer = true,
-  color_overrides = {
-    mocha = {
-      surface1 = "#9499b5",
-    },
-  },
   custom_highlights = function(colors)
     local U = require("catppuccin.utils.colors")
     return {
       Pmenu = { bg = U.darken(colors.surface0, 0.5, colors.crust) },
-      PmenuSel = { fg = colors.crust },
       NormalFloat = { bg = U.darken(colors.surface0, 0.5, colors.crust) },
     }
   end,
